c79a5a164d8f9c377412997756a55baf53064009
Richie <richie@Richies-MacBook-Pro.local>
2022-03-22 00:00:53 -0800

    atttempt

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: process.env.HEROKU_POSTGRESQL_IVORY_URL,
     pool: {
       min: 2,
       max: 10,
