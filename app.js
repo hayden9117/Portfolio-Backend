171d3295f5dc002cf7cee079c93f2d63ae66aa84
Richie <richie@Richies-MacBook-Pro.local>
2022-03-23 16:36:58 -0800

    cors change

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ const corsOptions = {
       callback(new Error("Not allowed by CORS"));
     }
   },
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
 app.use(cors(corsOptions));
