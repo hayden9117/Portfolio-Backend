de0621d2d74c827aaa7cf574abf20d533329daae
Richie <richie@Richies-MacBook-Pro.local>
2022-04-03 16:29:55 -0800

    domain change

--- app.js
+++ app.js
@@ -14,24 +14,25 @@ var updateAmazonData = require("./routes/updateAmazonData");
 var updateUser = require("./routes/updateUser");
 var addProductWeek = require("./routes/addProductWeek");
 var getProductWeek = require("./routes/getProductWeek");
+const { port } = require("pg/lib/defaults");
 // require("dotenv").config();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 // SERVER
-// app.listen(PORT || 3001, () => {
-//   console.log("running");
-// });
 
 setInterval(updateAmazonData, 1000 * 60 * 60);
 setInterval(addProductWeek, 1000 * 60 * 120);
 // Middleware
 app.use(
   cors({
-    origin: "https://richiehayden-portfolio-fronten.herokuapp.com",
+    origin: "https://www.haydenportfol.io/",
+    // origin: 3000,
     credentials: true,
   })
 );
-
+app.listen(port, () => {
+  console.log("running");
+});
 // const whitelist = [
 //   "http://localhost:3001",
 //   "http://localhost:8080",
@@ -66,34 +67,6 @@ app.get("/", (req, res) => {
 });
 
 // docker run --rm --name pg-docker -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v ~/docker/volumes/postgres:/var/lib/postgresql/data postgres
-
-// app.post('/login', async (req, res) => {
-//   let userName = req.body.username
-//   let passWord = req.body.password
-
-//   knex
-//     .select('*')
-//     .from('accounts')
-//     .where({ username: `${userName}` })
-//     .then(result => {
-//       if (result.length === 0) {
-//         res.send({ message: 'No match found, create new user.' })
-//       } else if (result[0].password === passWord) {
-//         res.send({ message: 'login successful' })
-//       } else {
-//         res.send({ message: 'incorrect password' })
-//       }
-
-//     }
-//     )
-// .then(data => res.status(200).json(data))
-// .catch(err => res.status(404).json({ message: 'these are not the users you are looking for' }))
-
-// {username: 'username', password: 'password'}
-
-//   console.log(`user ${userName} pass ${passWord} result: $ ${result[0].password} `)
-
-// })
 
 app.get("/test", (req, res) => {
   console.log("request records");
@@ -160,67 +133,4 @@ app.get("/amazonWeekTest", function (req, res, next) {
     });
 });
 
-/* GET users listing. */
-// router.get("/", function (req, res, next) {
-//   console.log("request records");
-//   knex
-//     .select("*")
-//     .from("Amazon")
-//     .orderBy("id")
-//     .then((data) => res.status(200).json(data))
-//     .catch((err) => {
-//       res.status(404).json(
-//         {
-//           message: "not found",
-//         },
-//         console.log(err.message)
-//       );
-//     });
-// });
-// app.post('/newuser', (req, res) => {
-//   knex
-//     .select('*')
-//     .from('accounts')
-//     .where({ username: `${req.body.username}` })
-//     .then(result => {
-//       if (result.length === 0) {
-//         // create the new user
-//         knex.insert({
-//           username: req.body.username,
-//           password: req.body.password,
-//         }).into('accounts')
-//           .then(res.send({ message: 'successfully added new entry to database' }))
-
-//       } else {
-//         res.send({ message: 'User Already Exists' })
-//       }
-
-//     }
-//     )
-//     .catch(err => res.send({ message: 'error when trying to add user' }))
-
-// })
-
-// app.post('/update', (req, res) => {
-//   console.log(req.body)
-//   knex('scores')
-//     .where({ username: req.body.username })
-//     .update({ username: req.body.newusername })
-//     .update({ password: req.body.newpassword })
-//     .then(res.send({ message: 'username updated' }))
-//     .catch(err => res.send({ message: 'error when updating username' }))
-// })
-
-// app.delete('/delete', (req, res) => {
-//   console.log('user tried to delete', req.body.username)
-//   knex('scores')
-//     .del()
-//     .where({ username: req.body.username })
-//     .then(data => res.status(200).json(data))
-//     .catch(err => {
-//       res.status(404).json({
-//         message: 'not found'
-//       })
-//     })
-// })
 module.exports = app;
