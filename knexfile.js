b609f53f23b9ea12762bae6d930cc09bdf20a199
Richie <richie@Richies-MacBook-Pro.local>
2022-07-01 18:41:31 -0800

    multiple changes

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,12 @@
 // Update with your config settings.
 const connectionString = process.env.HEROKU_POSTGRESQL_IVORY_URL;
 module.exports = {
-  development: {
-    client: "pg",
-    connection: "postgres://postgres:docker@localhost/",
-    migrations: { directory: "./migrations" },
-    seeds: { directory: "./seeds" },
-  },
+  // development: {
+  //   client: "pg",
+  //   connection: "postgres://postgres:docker@localhost/",
+  //   migrations: { directory: "./migrations" },
+  //   seeds: { directory: "./seeds" },
+  // },
   production: {
     client: "pg",
     connection: {
